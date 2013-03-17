7bdd924b8804c775252e1ab2fbf308499973dd40
edyother <ed@edyother.com>
2013-03-17 10:47:44 -0400

    update

--- rsketch.sh
+++ rsketch.sh
@@ -5,12 +5,13 @@
 
 TIME=$(date +%Y-%m-%d_%T)
 NAME=$TIME"$1"
+mkdir -p $1
 clear
 figlet RECORDING
 figlet "$1"
 echo "CTRL+C to stop"
-rec $TIME.ogg
-sox $TIME.ogg $NAME.ogg trim 0.2 gain -n -1
-rm $TIME.ogg
+rec ./$1/$TIME.ogg
+sox ./$1/$TIME.ogg ./$1/$NAME.ogg trim 0.2 gain -n -1
+rm ./$1/$TIME.ogg
 clear
-ls -1 *.ogg
+ls -1 ./$1/*.ogg
