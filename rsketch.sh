d04fa597d0b0d5ad76de5d43829dfea4935c974e
edyother <ed@edyother.com>
2013-03-18 16:52:53 -0400

    update

--- rsketch.sh
+++ rsketch.sh
@@ -6,7 +6,6 @@
 # set variables
 TIME=$(date +%Y-%m-%d_%T)
 NAME=$TIME"$1"
-FIX="sox $1/$TIME.ogg $1/$NAME.ogg trim 0.2 highpass 90 compand 0.3,0.8 6:-50,-50,-30 -3 -90 0.3 gain -n -3 gain -n -1"
 
 # Make directory if necessary
 mkdir -p $1
@@ -33,11 +32,11 @@ echo "--------------------------"
 read C
 if [ $C = k ]
   then
-  $FIX
+  sox $1/$TIME.ogg $1/$NAME.ogg trim 0.2 highpass 90 compand 0.3,0.8 6:-50,-50,-30 -3 -90 0.3 gain -n -3 gain -n -1
   ./rsketch.sh $1
-elif [ $C = kx ]
+elif [ $C = x ]
   then
-  $FIX
+  sox $1/$TIME.ogg $1/$NAME.ogg trim 0.2 highpass 90 compand 0.3,0.8 6:-50,-50,-30 -3 -90 0.3 gain -n -3 gain -n -1
 elif [ $C = d ]
   then 
   rm $1/$TIME.ogg
